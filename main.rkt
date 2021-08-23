ebaa007b40dc540c12f9e6564e8b64b9fd8cb3bd
Mark Bedaywi <50279151+Supermac30@users.noreply.github.com>
2021-08-22 22:13:00 -0400

    Add files via upload

--- main.rkt
+++ main.rkt
@@ -1,29 +1,37 @@
 #lang racket
 (provide (all-defined-out))
 
+; boolean expressions and operations
 (struct proposition (value) #:transparent)
 (struct conjunction (prop0 prop1) #:transparent)
 (struct disjunction (prop0 prop1) #:transparent)
 (struct implication (prop0 prop1) #:transparent)
 (struct negation (prop))
 
+; natural numbers and operations
+(struct zero () #:transparent)
+(struct succ (value) #:transparent)
+
 ; identifiers
-(struct start (statement proof) #:transparent)
-(struct end () #:transparent)
+(struct identifier () #:transparent)
+(struct start identifier (statement proof) #:transparent)
+(struct end identifier () #:transparent)
 
 ; expressions
-(struct get-var (name) #:transparent)
-(struct modus-ponens (hypo impl) #:transparent)
-(struct take (pos prop) #:transparent)
+(struct expression () #:transparent)
+(struct get-var expression (name) #:transparent)
+(struct modus-ponens expression (hypo impl) #:transparent)
+(struct take expression (pos prop) #:transparent)
 
 ; statements
-(struct take-conc (pos rest) #:transparent)
-(struct let (name prop rest) #:transparent)
-(struct apply (prop rest) #:transparent)
-(struct split (left right) #:transparent)
-(struct to-false (rest) #:transparent)
-(struct assume-hyp (name rest) #:transparent)
-(struct print-exp (prop rest) #:transparent) ; For the purpose of debugging
+(struct statement () #:transparent)
+(struct take-conc statement (pos rest) #:transparent)
+(struct let statement (name prop rest) #:transparent)
+(struct apply statement (prop rest) #:transparent)
+(struct split statement (left right) #:transparent)
+(struct to-false statement (rest) #:transparent)
+(struct assume-hyp statement (name rest) #:transparent)
+(struct print-exp statement (prop rest) #:transparent) ; For the purpose of debugging
 
 ; Warning: Works only on propositions without variable names, when only bools are stored on proposition
 (define (eval-prop prop)
@@ -36,7 +44,7 @@
                                  (eval-prop (implication-prop1 prop))
                                  true)]
         [(negation? prop) (if (eval-prop (negation-prop prop)) false true)]
-        [true (raise "Cannot call eval-prop without a proposition")]))
+        [else (raise "Cannot call eval-prop without a proposition")]))
 
 ; Negates without double negations. Not smart enough for Demorgan's laws (Add later?)
 (define (negate prop) (if (negation? prop) (negation-prop prop) (negation prop)))
@@ -121,9 +129,10 @@
 
 
 ; TODO
+; - Create documentation for each struct
 ; - Build tests for the function prove
-; - Create theorems that, after proven,
-;   can take in multiple assumptions and result in a new assumption (!)
+; - Create theorems that, after proven, can take in
+;   multiple assumptions and result in a new assumption (!)
 ; - Build a bank of theorems to be used in proofs
 
 ; - Add support for proofs by contradiction
@@ -137,3 +146,36 @@
 
 ; - Build a REPL for proving statements and building up theorems
 ;   - This would store proved theorems in a new file to be used again later.
+
+(struct type () #:transparent)
+(struct natural type () #:transparent)
+(struct boolean type () #:transparent)
+(struct to (type0 type1) #:transparent)
+(struct prod (type0 type1) #:transparent)
+
+(struct func (type arg body) #:transparent)
+(struct rec-func (name type arg base-case rec-case) #:transparent)
+(struct input (func arg) #:transparent)
+(struct left (arg) #:transparent)
+(struct right (arg) #:transparent)
+(struct pair (arg0 arg1) #:transparent)
+
+(define (eval-func func arg env)
+  (letrec ([eval-body (lambda (body env)
+                       (match body
+                         [(input func arg) (eval-func (eval-body func env) (eval-body arg env) env)]
+                         [(succ num) (succ (eval-body num env))]
+                         [(get-var name) (hash-ref env name)]
+                         [(left arg) (car (eval-body arg env))]
+                         [(right arg) (cdr (eval-body arg env))]
+                         [(pair arg0 arg1) (cons (eval-body arg0 env) (eval-body arg1 env))]
+                         [_ body]))])
+    (cond [(func? func) (eval-body (func-body func) (hash-set env (func-arg func) arg))]
+          [(rec-func? func) (match (rec-func-type func)
+                              [(to (natural) _) (match arg
+                                                  [(zero) (eval-body (rec-func-base-case func) env)]
+                                                  [(succ n) (eval-body (rec-func-rec-case func)
+                                                                       (hash-set (hash-set env (rec-func-name func) func)
+                                                                                 (rec-func-arg func) n))])]
+                              [_ (raise (to-type0 (rec-func-type func)))])]
+          [true (raise "Can only evaluate functions")])))
---
+++ natural_numbers.rkt
@@ -0,0 +1,61 @@
+#lang racket
+; Holds functions and proofs for natural numbers
+(require "main.rkt")
+
+(define add-func
+  (func (to (prod (natural) (natural)) (natural))
+        "nums"
+        (input (rec-func "add-n"
+                         (to (natural) (natural))
+                         "n"
+                         (left (get-var "nums"))
+                         (succ (input (get-var "add-n")
+                                      (get-var "n"))))
+               (right (get-var "nums")))))
+
+(define (add num0 num1)
+  (input add-func (pair num0 num1)))
+
+(define double-func
+  (func (to (natural) (natural))
+        "num"
+        (add (get-var "num") (get-var "num"))))
+
+(define (double n)
+  (input double-func n))
+
+(define power-of-2-func
+  (rec-func "power-of-2-func"
+            (to (natural) (natural))
+            "n"
+            (succ (zero))
+            (double (input (get-var "power-of-2-func")
+                           (get-var "n")))))
+
+(define mult-func
+  (func (to (prod (natural) (natural)) (natural))
+        "nums"
+        (input (rec-func "mult-n"
+                         (to (natural) (natural))
+                         "n"
+                         (zero)
+                         (add (input (get-var "mult-n") (get-var "n"))
+                              (left (get-var "nums"))))
+               (right (get-var "nums")))))
+
+(define (mult num0 num1)
+  (input mult-func (pair num0 num1)))
+
+(define pow-func
+  (func (to (prod (natural) (natural)) (natural))
+        "nums"
+        (input (rec-func "mult-n"
+                         (to (natural) (natural))
+                         "n"
+                         (succ (zero))
+                         (mult (input (get-var "mult-n") (get-var "n"))
+                               (left (get-var "nums"))))
+               (right (get-var "nums")))))
+
+(define (pow num0 num1)
+  (input pow-func (pair num0 num1)))
